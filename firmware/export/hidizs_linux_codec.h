269156146e0374920b2df35e18676169b9b7893b
Georgii Surkov <georgii.surkov@outlook.com>
2026-05-31 22:49:44 +0100

    target: ap80prox: implement playback (stub)

--- firmware/export/hidizs_linux_codec.h
+++ firmware/export/hidizs_linux_codec.h
@@ -1,7 +1,33 @@
 #ifndef _HIDIZS_LINUX_CODEC_
 #define _HIDIZS_LINUX_CODEC_
 
-// TODO: Figure out the real values
-AUDIOHW_SETTING(VOLUME, "dB", 1, 5, -102*10,  0, -30*10)
+/* 1 dB = 10 cB (centibels) */
+#define DB_TO_CB(db) ((db) * 10)
+
+/* No influence on volume */
+#define VOLUME_UNITS      "dB"
+/* The number of digits - 1 of the volume value */
+#define VOLUME_EXPONENT   1
+/* Volume step in centibels */
+#define VOLUME_STEP_CB    5
+/* Maximum volume in centibels */
+#define VOLUME_MAX_CB     DB_TO_CB(0)
+/* Minimum volume in centibels */
+#define VOLUME_MIN_CB     DB_TO_CB(-102)
+/* Default volume in centibels */
+#define VOLUME_DEFAULT_CB DB_TO_CB(-30)
+
+AUDIOHW_SETTING(\
+    VOLUME, \
+    VOLUME_UNITS, \
+    VOLUME_EXPONENT, \
+    VOLUME_STEP_CB, \
+    VOLUME_MIN_CB, \
+    VOLUME_MAX_CB, \
+    VOLUME_DEFAULT_CB)
+
+#define AUDIOHW_NEEDS_INITIAL_UNMUTE
+
+void audiohw_mute(int mute);
 
 #endif /* _HIDIZS_LINUX_CODEC_ */
